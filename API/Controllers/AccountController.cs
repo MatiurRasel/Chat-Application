bd5d4404b2bab7cf84821f31086ea3ed8e1ffee3
Matiur Rasel <matiur@deshuniversal.com>
2023-09-17 22:59:05 +0600

    add-changes

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -53,7 +53,8 @@ namespace API.Controllers
             {
                 UserName = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
 
         }
@@ -86,7 +87,8 @@ namespace API.Controllers
                 UserName = user.UserName,
                 Token = _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 

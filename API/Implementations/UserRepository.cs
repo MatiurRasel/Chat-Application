1271c7122e8a9cf49582071e3fc847a3e07c63d8
Matiur Rasel <matiur@deshuniversal.com>
2023-09-21 23:24:42 +0600

    add-changes

--- API/Implementations/UserRepository.cs
+++ API/Implementations/UserRepository.cs
@@ -49,6 +49,12 @@ namespace API.Implementations
             
             query = query.Where(u=>u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
 
+            query = userParams.OrderBy switch
+            {
+                "created" => query.OrderByDescending(u=>u.Created),
+                _ => query.OrderByDescending(u=>u.LastActive)
+            };
+
                 return await PagedList<MemberDTO>.CreateAsync(
                     query.AsNoTracking().ProjectTo<MemberDTO>(_mapper.ConfigurationProvider),
                     userParams.PageNumber,userParams.PageSize);

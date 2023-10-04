59dc5b1c220cd14c8dced7fa50d4589a26c71991
MatiurRasel <matiurrasel1002@gmail.com>
2023-10-04 18:04:28 +0600

    add-changes

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -12,6 +12,7 @@ namespace API.Data
 
         public DbSet<AppUser> Users { get; set; }
         public DbSet<UserLike> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -38,6 +39,11 @@ namespace API.Data
             // .WithMany(l=>l.LikedByUsers)
             // .HasForeignKey(s=>s.TargetUserId)
             // .OnDelete(DeleteBehavior.NoAction);
+
+            
+
+
+
             
 
         }

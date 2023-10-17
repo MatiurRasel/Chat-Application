b83b8526984fb87f86e1715c74dfbc22af3b6ce9
MatiurRasel <matiurrasel1002@gmail.com>
2023-10-17 15:25:35 +0600

    add-changes-final

--- API/DTOS/PhotoDTO.cs
+++ API/DTOS/PhotoDTO.cs
@@ -10,5 +10,6 @@ namespace API.DTOS
         public int Id {get;set;}
         public string Url {get;set;}
         public bool IsMain {get;set;}
+        public bool IsApproved { get; set; }
     }
 }
--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -13,9 +13,11 @@ namespace API.Interfaces
         Task<AppUser> GetUserByUserNameAsync(string userName);
 
         Task<PagedList<MemberDTO>> GetMembersAsync(UserParams userParams);
+        Task<MemberDTO> GetMemberAsync(string userName, bool isCurrentUser);
         Task<MemberDTO> GetMemberByIdAsync(int id);
         Task<MemberDTO> GetMemberByUserNameAsync(string userName);
         Task<string> GetUserGender(string userName);
+        Task<AppUser> GetUserByPhotoId(int photoId);
 
     }
 }
